0d728f05d729ae523183fcb58b06e578fe668a57
Alicia-C <96775273+C-Alicia@users.noreply.github.com>
2025-10-13 14:40:38 +0200

    feat: add router for gestion route

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="header">
+        <RouterLink to="/" class="header__logo">
+            <img :src="logoLight" alt="Logo du site" width="15%" />
+        </RouterLink>
+        <nav>
+            <RouterLink to="/propos">A propos</RouterLink>
+            <RouterLink to="/parcours">Mon parcours</RouterLink>
+            <RouterLink to="/projets">Projets / Réalisations</RouterLink>
+            <RouterLink to="/blog">Blog</RouterLink>
+            <RouterLink to="/contact">Contact</RouterLink>
+        </nav>
+    </header>
+</template>
+
+
+<script setup>
+import logoLight from '../assets/img/logo_light.svg'
+import logoDark from '../assets/img/logo_dark.svg'
+</script>
+
+
+<style>
+.header {
+    display: flex;
+    align-items: center;
+    padding: .5rem 1rem;
+    gap: 1rem;
+    background-color: #ffffff10;
+    border-bottom: 1px solid #ffffff2d;
+}
+</style>
